b77539f0f251d1202029251890d966d63383e0f8
Zach Kagin <zkagin@gmail.com>
2019-01-26 11:37:30 -0500

    Initial Commit

---
+++ SecureBlock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecureBlock
+//
+//  Created by Zach Kagin on 1/26/19.
+//  Copyright © 2019 Zach Kagin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
